710898abb0e1725cfa67cb9093175b340721ec89
Thiago Campanille <thiago.reis@amazonasinovare.com.br>
2026-04-07 18:28:48 -0300

    :sparkles: feat: implement global validation pipe and add class-validator decorators to task DTOs

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,12 @@
+import { IsBoolean, IsOptional, IsString, IsNotEmpty } from "class-validator";
+
 export class UpdateTaskDto {
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
     title?: string;
+
+    @IsOptional()
+    @IsBoolean()
     completed?: boolean;
 }
