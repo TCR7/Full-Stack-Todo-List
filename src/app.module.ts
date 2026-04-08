82b5b66b064985fd1d105a1def9cded59ce4b901
Thiago Campanille <thiago.reis@amazonasinovare.com.br>
2026-04-08 14:35:37 -0300

    :sparkles: feat: add Prisma ORM and SQLite persistence to tasks API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TaskModule } from './task/task.module';
-import { TasksController } from './tasks/tasks.controller';
-import { TasksService } from './tasks/tasks.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TaskModule],
-  controllers: [AppController, TasksController],
-  providers: [AppService, TasksService],
+  imports: [TasksModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,21 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import {Task} from './tasks.interface';
-import {CreateTaskDto} from './dto/create.task.dto';
-import {UpdateTaskDto} from './dto/update-task.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateTaskDto } from './dto/create.task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [
-        {id: 1, title: "Estudar NestJS", completed: false},
-        {id: 2, title: "Criar projeto To-Do-List", completed: true}
-    ];
+    constructor(private readonly prisma: PrismaService) {}
 
-    findAll(): Task[] {
-        return this.tasks;
+    findAll() {
+        return this.prisma.task.findMany();
     }
 
-    findOne(id: number): Task {
-        const task = this.tasks.find(task => task.id === id);
+    async findOne(id: number) {
+        const task = await this.prisma.task.findUnique({
+            where: { id },
+        });
 
         if(!task) {
             throw new NotFoundException(`Task com o id ${id} não encontrada`);
@@ -24,36 +23,28 @@ export class TasksService {
         return task;
     }
 
-    create(createTaskDto: CreateTaskDto): Task{
-        const newTask: Task =  {
-            id: this.tasks.length + 1,
-            title: createTaskDto.title,
-            completed: false,
-        };
-        this.tasks.push(newTask);
-
-        return newTask;
+    create(createTaskDto: CreateTaskDto){
+        return this.prisma.task.create({
+            data: {
+                title: createTaskDto.title,
+            },
+        });
     }
 
-    update(id: number, updateTaskDto: UpdateTaskDto): Task {
-        const task = this.findOne(id);
+    async update(id: number, updateTaskDto: UpdateTaskDto) {
+        await this.findOne(id);
 
-       if(updateTaskDto.title !== undefined){
-        task.title = updateTaskDto.title;
-       }
-
-       if(updateTaskDto.completed !== undefined) {
-        task.completed = updateTaskDto.completed;
-       }
-
-       return task;
+        return this.prisma.task.update({
+            where: { id },
+            data: updateTaskDto,
+        });
     }
 
-    delete(id: number): Task {
-        const task = this.findOne(id);
+    async delete(id: number) {
+        await this.findOne(id);
 
-        this.tasks = this.tasks.filter((task) => task.id !== id);
-
-        return task;
+        return this.prisma.task.delete({
+            where: { id },
+        });
     }
 }
